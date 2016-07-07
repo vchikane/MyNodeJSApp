e98b4343561d9068d9d72baef643ae970b450ef6
vipulruparelia <vipul.ruparelia@tcs.com>
2016-07-07 18:21:23 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/create', function(req, res) {
 router.post('/create/add', function(req, res) {
 	//generating unique key using currect time
 	var date =  Math.floor(Date.now()/1000);
-	var key_name = 'user_'+date;
+	var key_name = 'user_' + date;
 	console.log(key_name);
 
 	obj = {
@@ -29,7 +29,7 @@ router.post('/create/add', function(req, res) {
 	client.hmset('users', key_name, JSON.stringify(obj), function(err,doc) {
 		res.send('User created successfully');
 	});
-	
+
 });
 
 router.get('/users', function(req, res) {
@@ -39,5 +39,63 @@ router.get('/users', function(req, res) {
 		})
 	})
 });
+
+/* Get Modify User Page */
+router.get('/edit_users',function(req,res){
+	console.log("edit user")
+	key=req.query.u_id;
+	console.log(key);
+	client.hget('users', key, function(err,data){
+		console.log(data)
+		//below code will rendor modify details page with all details
+		res.render('update',{
+			user : data,
+			key : key
+		}) 
+	})
+});
+
+/* Update User Details */
+router.post('/modify/change', function(req, res) {
+	key_1 = req.body.key;
+	console.log("key is " + key);
+	obj = {
+		'empid'  : req.body.empid, 
+		'name' 	 : req.body.name,
+		'mobile' : req.body.mobile
+	}
+	console.log("type of obj " + typeof(obj));
+	console.log("value of obj " + obj);
+	client.hmset('users', key_1, JSON.stringify(obj), function(err, doc) {
+		res.send("User Detais Updated successfully");
+	})
+	
+});
+
+/* Get Delete User Page */
+router.get('/delete_user',function(req,res){
+	console.log("delete user")
+	key=req.query.u_id;
+	client.hget('users', key, function(err,data){
+		console.log(data)
+		res.render('delete',{
+			user : data,
+			key : key
+		})
+	})
+});
+
+/* Delete User */
+router.post('/delete/remove', function(req, res) {
+	key_1 = req.body.key;
+	console.log(key_1);
+	client.hdel('users', key_1, function() {
+		res.send("User Deleted successfully");
+	})
+	
+});
+
+
+
 
 module.exports = router;
